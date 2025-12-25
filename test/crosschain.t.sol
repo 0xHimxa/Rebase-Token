8b793aa95811fc97e38a00f1924de9301f033145
himxa <himxa0x@gmail.com>
2025-12-25 02:50:47 +0100

    enhance cross-chain test setup; add network details and register admin roles for RebaseToken and pools

--- test/crosschain.t.sol
+++ test/crosschain.t.sol
@@ -8,7 +8,18 @@ import {Vault} from "../src/vault.sol";
 import {IERC20} from "lib/ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {TokenPool} from "lib/ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 import {Pool} from "lib/ccip/contracts/src/v0.8/ccip/libraries/Pool.sol";
-import {CCIPLocalSimulatorFork} from "lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {CCIPLocalSimulatorFork,Register} from "lib/chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {
+    IERC20
+} from "lib/ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+
+import {RegistryModuleOwnerCustom} from "lib/ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+
+
+//register is a  store struct that contains the roter,selector and cip test token addree
+//click the file for more info
+
+
 
 contract CrossChainTest is Test {
     RebaseToken  sepoliaToken;
@@ -18,6 +29,13 @@ contract CrossChainTest is Test {
     uint256 private sepoliaFork;
     uint256 private arbSepoliaFork;
     CCIPLocalSimulatorFork private ccipLocalSimulatorFork;
+    RebaseTokenPool sepoliaPool;
+    RebaseTokenPool arbSepoliaPool;
+
+    Register.NetworkDetails sepoliaNetworkDetails;
+    Register.NetworkDetails arbSepoliaNetworkDetails;
+
+
 
  address owner = makeAddr('owner');
 function setUp() external{
@@ -39,17 +57,49 @@ vm.makePersistent(address(ccipLocalSimulatorFork));
 // below we deply it on two chain
 
 //deploy and configure contracts on sepolia fork
+
+
+
+sepoliaNetworkDetails =ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+arbSepoliaNetworkDetails =ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
 vm.startPrank(owner);
-sepoliaToken  = mew RebaseToken();
+sepoliaToken  = new RebaseToken();
 
 //we only deploy vault on sepolia, it our soure chain
 vault = new Vault(address(sepoliaToken));
+sepoliaPool = new RebaseTokenPool(IERC20(address(sepoliaToken)),
+    new address[](0),
+    sepoliaNetworkDetails.rmnProxyAddress,
+    sepoliaNetworkDetails.routerAddress,
+    address(sepoliaToken)
+    );
+
+   sepoliaToken.grandtMintAndBurnRole(address(vault)); 
+    sepoliaToken.grandtMintAndBurnRole(address(sepoliaPool));
+
+    RegistryModuleOwnerCustom(sepoliaNetworkDetails.registryModuleOwnerCustomAddress).registerAdminViaOwner(address(sepoliaToken));
+
 vm.stopPrank();
 
+
+arbSepoliaNetworkDetails =ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
 //2nd switch to arb sepolia fork and deploy and configure contracts there
 vm.selectFork(arbSepoliaFork);
 vm.startPrank(owner);
 arbSepoliaToken  = new RebaseToken();
+
+
+arbSepoliaPool = new RebaseTokenPool(IERC20(address(arbSepoliaToken)),
+    new address[](0),
+   arbSepoliaNetworkDetails.rmnProxyAddress,
+    arbSepoliaNetworkDetails.routerAddress,
+    address(arbSepoliaToken)
+    );
+
+    arbSepoliaToken.grandtMintAndBurnRole(address(arbSepoliaPool));
+    arbSepoliaToken.grandtMintAndBurnRole(address(vault));
+
+    RegistryModuleOwnerCustom(arbSepoliaNetworkDetails.registryModuleOwnerCustomAddress).registerAdminViaOwner(address(arbSepoliaToken));
 vm.stopPrank();
 
    
